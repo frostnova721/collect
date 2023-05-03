24a0faabd896da4d0bd3a17f77ff8587eec4ff6a
Pikachu <pikachuandbatman5891@gmail.com>
2023-05-03 19:52:28 +0530

    Initial commit

---
+++ collector.js
@@ -0,0 +1,35 @@
+const qrcode = require("qrcode-terminal");
+const { Client, LocalAuth, MessageMedia } = require("whatsapp-web.js");
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+});
+
+client.on('ready', () => {
+    console.log('Ready To Run')
+})
+
+client.on("qr", (qr) => {
+    qrcode.generate(qr, { small: true });
+  });
+
+client.on('message', (message) => {
+    console.log(message.body)
+})
+
+client.on('message', (message) => {
+    if(message.body.includes('A Collectable card Appeared!') && message.body.includes(`*Tier:* 4`)) {
+        message.reply('!claim')
+    }
+    else if(message.body.includes('A Collectable card Appeared!') && message.body.includes(`*Tier:* 5`)) {
+        message.reply('!claim')
+    }
+    else if(message.body.includes('A Collectable card Appeared!') && message.body.includes(`*Tier:* 6`)) {
+        message.reply('!claim')
+    }
+    else if(message.body.includes('An S tier Card Appeared!')) {
+        message.reply('!claim')
+    }
+})
+
+client.initialize()
